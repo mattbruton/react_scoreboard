fbed659ab4dcea5f297b31af28e3386b47fdf16f
MATT BRUTON <the.bruton@gmail.com>
2017-02-08 09:54:07 -0600

    added index.js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//import './styles/main.scss';
+
+export default class App extends React.Component {
+
+  render() {
+    return(
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
