2d5dde70e6e12d8e2336671dbbf26b180735a974
MATT BRUTON <the.bruton@gmail.com>
2017-02-10 10:40:52 -0600

    had issue loading scss files, reinstalled sass-loader and node-sass

--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//import './styles/main.scss';
+import './styles/main.scss';
 
 export default class App extends React.Component {
 
   render() {
     return(
-      <div>
-        
+      <div className="scoreboard">
+        <div className="header">
+          <h1>{this.props.title}</h1>
+        </div>
+        <div className="players">
+          <div className="player">
+            <div className="player-name">
+              Matt
+            </div>
+            <div className="player-score">
+              <div className="counter">
+                <button className="counter-action decrement"> - </button>
+                <div className="counter-score"> 31 </div>
+                <button className="counter-action increment"> + </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
+App.propTypes = {
+  title: React.PropTypes.string
+};
+
 ReactDOM.render(
-  <App />,
+  <App title="My Scoreboard"/>,
   document.getElementById('app')
 );
